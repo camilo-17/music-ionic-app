4a727e56dd02072ef48f8732f53f13752b85a3e9
Camilo <camilo.caro@datawifi.co>
2022-01-30 19:56:30 -0500

    [BUG] fix maps dependecies key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: `https://platzi-music-api.herokuapp.com`};
+  apiURL: `https://platzi-music-api.herokuapp.com`
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  apiURL: `https://platzi-music-api.herokuapp.com`,
-  mapsKeyApi: "AIzaSyA6VHJsv00ZPYJruJKqeiF0dKXcb90zk0A"
+  apiURL: `https://platzi-music-api.herokuapp.com`
 };
 
